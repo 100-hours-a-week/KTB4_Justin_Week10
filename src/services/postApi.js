38c67edb9b7b40a6525526c362f5ed7244bda472
DDONG <shet6006@gmail.com>
2026-08-09 18:45:35 +0900

    feat: 게시글 검색 및 자동완성 API 연결

--- src/services/postApi.js
+++ src/services/postApi.js
@@ -1,6 +1,6 @@
 import { apiRequest } from './apiClient.js'
 
-function createPageQuery({ page = 0, size = 10, genre = '' } = {}) {
+function createPageQuery({ page = 0, size = 10, genre = '', keyword = '' } = {}) {
   const params = new URLSearchParams({
     page: String(page),
     size: String(size),
@@ -8,6 +8,10 @@ function createPageQuery({ page = 0, size = 10, genre = '' } = {}) {
 
   if (genre) {
     params.set('genre', genre)
+  }
+
+  if (keyword) {
+    params.set('keyword', keyword)
   }
 
   return params
@@ -24,6 +28,15 @@ export const getLikedPosts = (pageOptions = {}) => {
   const params = createPageQuery(pageOptions)
 
   return apiRequest(`/posts/liked?${params.toString()}`, 'GET')
+}
+
+export const getPostSuggestions = ({ keyword, size = 5 }) => {
+  const params = new URLSearchParams({
+    keyword,
+    size: String(size),
+  })
+
+  return apiRequest(`/posts/suggestions?${params.toString()}`, 'GET')
 }
 
 export const getPost = (postId) => {
